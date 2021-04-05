8aaa10b7d650f309ba097b4d7458b13233964795
Oscar Lima <oscar.lima@dfki.de>
2021-04-05 17:32:10 +0200

    multiple changes in pick script, now grasping for the first time a cylinder

--- april_pick_place_object/src/pick.py
+++ april_pick_place_object/src/pick.py
@@ -29,6 +29,16 @@ class PickTools():
         self.pick_attempts = rospy.get_param('~pick_attempts', 5)
         object_name = None
         self.object_padding = rospy.get_param('~object_padding', 0.04)
+        self.grasp_quality = rospy.get_param('~grasp_quality', 1.0)
+        # pregrasp parameters
+        self.pre_grasp_approach_min_dist = rospy.get_param('~pre_grasp_approach/min_dist', 0.08)
+        self.pre_grasp_approach_desired = rospy.get_param('~pre_grasp_approach/desired', 0.25)
+        self.pre_grasp_approach_axis = rospy.get_param('~pre_grasp_approach/axis', [0.0, 1.0, 0.0])
+        # post grasp retreat parameters
+        self.post_grasp_retreat_frame_id = rospy.get_param('~post_grasp_retreat/frame_id', 'world')
+        self.post_grasp_retreat_min_dist = rospy.get_param('~post_grasp_retreat/min_dist', 0.1)
+        self.post_grasp_retreat_desired = rospy.get_param('~post_grasp_retreat/desired', 0.15)
+        self.post_grasp_retreat_axis = rospy.get_param('~post_grasp_retreat/axis', [0.0, 0.0, 1.0])
 
         if not gripper_close or not gripper_open:
             raise ValueError('mandatory parameters gripper_close or/and gripper_open not set')
@@ -133,6 +143,7 @@ class PickTools():
         trajectory_point = JointTrajectoryPoint()
         trajectory_point.positions = joint_angles
         trajectory_point.effort = self.gripper_joint_efforts
+        trajectory_point.time_from_start = rospy.Duration(1.0)
         trajectory.points.append(trajectory_point)
         return trajectory
 
@@ -146,10 +157,10 @@ class PickTools():
         gripper_translation_msg = GripperTranslation()
 
         # the direction of the translation
+        gripper_translation_msg.direction.header.frame_id = frame_id
         gripper_translation_msg.direction.vector.x = axis[0]
         gripper_translation_msg.direction.vector.y = axis[1]
         gripper_translation_msg.direction.vector.z = axis[2]
-        gripper_translation_msg.direction.header.frame_id = frame_id
 
         # the desired translation distance
         gripper_translation_msg.desired_distance = desired
@@ -159,7 +170,7 @@ class PickTools():
 
         return gripper_translation_msg
 
-    def make_grasps_msgs(self, object_name, grasp_pose, grasp_quality=1.0):
+    def make_grasps_msgs(self, object_name, grasp_pose):
         '''
         generate grasp configurations for moveit
         grasp_pose : The position of the end-effector for the grasp
@@ -171,14 +182,15 @@ class PickTools():
 
         # The estimated probability of success for this grasp, or some other
         # measure of how 'good' it is.
-        g.grasp_quality = grasp_quality
+        g.grasp_quality = self.grasp_quality
 
-        # The internal posture of the hand for the grasp
-        # positions and efforts are used
-        g.grasp_posture = self.gripper_open_trajectory
+        # The internal posture of the hand for the pre-grasp
+        # only positions are used
+        g.pre_grasp_posture = self.gripper_open_trajectory
 
         # The approach direction to take before picking an object
-        g.pre_grasp_approach = self.make_gripper_translation_msg(self.arm.get_end_effector_link(), min_dist=0.08, desired=0.25, axis=[1.0, 0.0, 0.0])
+        g.pre_grasp_approach = self.make_gripper_translation_msg(self.arm.get_end_effector_link(),
+            min_dist=self.pre_grasp_approach_min_dist, desired=self.pre_grasp_approach_desired, axis=self.pre_grasp_approach_axis)
 
         # The position of the end-effector for the grasp.  This is the pose of
         # the 'parent_link' of the end-effector, not actually the pose of any
@@ -186,12 +198,13 @@ class PickTools():
         # most distal wrist link before the hand (end-effector) links began.
         g.grasp_pose = grasp_pose
 
-        # The internal posture of the hand for the pre-grasp
-        # only positions are used
-        g.pre_grasp_posture = self.gripper_close_trajectory
+        # The internal posture of the hand for the grasp
+        # positions and efforts are used
+        g.grasp_posture = self.gripper_close_trajectory
 
         # The retreat direction to take after a grasp has been completed (object is attached)
-        g.post_grasp_retreat = self.make_gripper_translation_msg('world', min_dist=0.1, desired=0.15, axis=[0.0, 0.0, 1.0])
+        g.post_grasp_retreat = self.make_gripper_translation_msg(self.post_grasp_retreat_frame_id,
+            min_dist=self.post_grasp_retreat_min_dist, desired=self.post_grasp_retreat_desired, axis=self.post_grasp_retreat_axis)
 
         # the maximum contact force to use while grasping (<=0 to disable)
         g.max_contact_force = 1.0
@@ -218,8 +231,8 @@ class PickTools():
         target_pose = PoseStamped()
 
         # object in cartesian coordinates (is also recorded as "object" posture in srdf)
-        translation = [-0.009323, 0.665396, 1.162619]
-        rotation = [-0.351800, 0.511656, -0.391641, -0.679014]
+        translation = [-0.000950, 0.682979, 1.158076]
+        rotation = [-0.116526, 0.609924, -0.207705, -0.755825]
 
         # home posture in cartesian coordinates
         #translation = [-0.288703, 0.154675, 1.592202]
@@ -268,6 +281,9 @@ class PickTools():
             rospy.logerr('object recognition has not detected any objects')
             return
 
+        # move arm to pregrasp in joint space, not really needed, can be removed
+        self.move_arm_to_posture('home')
+
         # ::::::::: setup planning scene
         rospy.loginfo('setup planning scene')
 
@@ -275,19 +291,19 @@ class PickTools():
         self.clean_scene()
 
         # add a table to the planning scene
-        #table_pose = PoseStamped()
-        #table_pose.header.frame_id = 'world'
-        #box_x = 1.39
-        #box_y = 0.85
-        #box_z = 1.0
-        #table_pose.pose.position.x = 0.06
-        #table_pose.pose.position.y = 0.69
-        #table_pose.pose.position.z = box_z / 2.0
-        #table_pose.pose.orientation.x = 0.0
-        #table_pose.pose.orientation.y = 0.0
-        #table_pose.pose.orientation.z = 0.0
-        #table_pose.pose.orientation.w = 1.0
-        #self.scene.add_box('table', table_pose, (box_x, box_y, box_z))
+        table_pose = PoseStamped()
+        table_pose.header.frame_id = 'world'
+        box_x = 1.39
+        box_y = 0.85
+        box_z = 1.0
+        table_pose.pose.position.x = 0.06
+        table_pose.pose.position.y = 0.69
+        table_pose.pose.position.z = box_z / 2.0
+        table_pose.pose.orientation.x = 0.0
+        table_pose.pose.orientation.y = 0.0
+        table_pose.pose.orientation.z = 0.0
+        table_pose.pose.orientation.w = 1.0
+        self.scene.add_box('table', table_pose, (box_x, box_y, box_z))
 
         # add an object to be grasped, data is comes from perception
         object_pose, bounding_box = self.make_object_pose(object_name, self.detections_msg)
@@ -302,13 +318,18 @@ class PickTools():
         # generate a list of moveit grasp messages
         grasp_pose = PoseStamped()
         grasp_pose.header.frame_id = 'world' # TODO: do it w.r.t object_name
-        grasp_pose.pose.position.x =     0.017
-        grasp_pose.pose.position.y =     0.674
-        grasp_pose.pose.position.z =     1.156
-        grasp_pose.pose.orientation.x =  0.352
-        grasp_pose.pose.orientation.y = -0.512
-        grasp_pose.pose.orientation.z =  0.392
-        grasp_pose.pose.orientation.w =  0.679
+
+        # object in cartesian coordinates
+        translation = [-0.000950, 0.682979, 1.158076]
+        rotation = [-0.116526, 0.609924, -0.207705, -0.755825]
+
+        grasp_pose.pose.position.x = translation[0]
+        grasp_pose.pose.position.y = translation[1]
+        grasp_pose.pose.position.z = translation[2]
+        grasp_pose.pose.orientation.x = rotation[0]
+        grasp_pose.pose.orientation.y = rotation[1]
+        grasp_pose.pose.orientation.z = rotation[2]
+        grasp_pose.pose.orientation.w = rotation[3]
         grasps = self.make_grasps_msgs(object_name, grasp_pose)
 
         # publish grasp pose for visualisation purposes
@@ -341,5 +362,5 @@ class PickTools():
 if __name__=='__main__':
     rospy.init_node('pick_object_node', anonymous=True)
     pick = PickTools()
-    #pick.start_pick_node()
-    pick.test_going_to_cartesian_pose()
+    pick.start_pick_node()
+    #pick.test_going_to_cartesian_pose()
